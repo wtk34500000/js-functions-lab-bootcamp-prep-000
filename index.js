8da2bfcf4f1c7e9f60a78ea76e89a9acbd15f62e
weijun huang <luck938938@hotmail.com>
2018-06-27 20:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function happyHolidayTo(holiday, name)
 
 function holidayCountdown(name, days)
 {
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It\'s ${days} days until ${holiday}!`
 }
